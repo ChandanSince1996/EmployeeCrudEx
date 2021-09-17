f2699d82f65258cfffe191bdef53dc68552b9407
ChandanSince1996 <nckr2014@gmail.com>
2021-09-17 23:30:21 +0530

    pom.xml and entity class modified

--- src/main/java/in/nareshit/raghu/entity/Employee.java
+++ src/main/java/in/nareshit/raghu/entity/Employee.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name="emptab")
 public class Employee {
@@ -22,48 +25,4 @@ public class Employee {
 	@Column(name="eaddr")
 	private String empAddr;
 	
-	public Employee(Integer id, String empName, Double empSal, String empDept, String empAddr) {
-		super();
-		this.id = id;
-		this.empName = empName;
-		this.empSal = empSal;
-		this.empDept = empDept;
-		this.empAddr = empAddr;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	public Double getEmpSal() {
-		return empSal;
-	}
-	public void setEmpSal(Double empSal) {
-		this.empSal = empSal;
-	}
-	public String getEmpDept() {
-		return empDept;
-	}
-	public void setEmpDept(String empDept) {
-		this.empDept = empDept;
-	}
-	public String getEmpAddr() {
-		return empAddr;
-	}
-	public void setEmpAddr(String empAddr) {
-		this.empAddr = empAddr;
-	}
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", empName=" + empName + ", empSal=" + empSal + ", empDept=" + empDept
-				+ ", empAddr=" + empAddr + "]";
-	}
-
 }
